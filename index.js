30e4d9508d796a47173e3267b9b853ab7f8d2130
Sergey Kozak <Si@Deep-Book.local>
2018-01-12 16:16:13 -0500

    added cross origin method

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ server.use(restify.plugins.bodyParser());
 server.use(
   function crossOrigin(req,res,next){
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     return next();
   }
